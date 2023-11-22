ca08eddeb13a1f78710115d7d33cbf7885c3af6c
marie <ingabiremarieaimee6@gmail.com>
2023-11-21 17:05:21 -0800

    me push

---
+++ index.js
@@ -0,0 +1,25 @@
+// Change code below this line
+const someAdjective = "i awesame in life ";
+let myStr = "Learning to code is "; 
+myStr += someAdjective;
+console.log("myStr")
+
+// Only change code below this line
+const myName = "My name is  "; 
+ myStr = "  and I am well!"+ myName +", my name is "
+
+ 
+ myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+ console.log("myStr")
+ myStr ="I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+ console.log("myStr")
+
+ myStr=3;
+ console.log("myStr")
+ let isTrue=4
+console.log(isTrue);
+
+ let num=3;
+ num =5;
+const bool = Boolean(num);
+console.log(bool);
